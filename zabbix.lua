a8ef0a0d4dbf92e90d2d1fe8ce63aa53ff9b3aa3
Pavel Shramov <psha@nguni.psha.org.ru>
2022-12-08 11:16:34 +0300

    examples: Update zabbix frame according to docs
    Explicitly set little-endian mode, size is only 4 bytes
    
    https://www.zabbix.com/documentation/current/en/manual/appendix/protocols/header_datalen

--- zabbix.lua
+++ zabbix.lua
@@ -2,11 +2,11 @@
 frame_size = 13
 
 function frame_pack(msg)
-	return string.pack("c5 I8", "ZBXD\x01", msg.size)
+	return string.pack("<c5 I4 I4", "ZBXD\x01", msg.size, 0)
 end
 
 function frame_unpack(frame, msg)
-	prefix, size = string.unpack("c5 I8", frame)
+	prefix, size = string.unpack("<c5 I4", frame)
 	msg.size = size
 	return frame_size
 end
