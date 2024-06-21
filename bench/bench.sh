70366d0b63af955256fd59e059cedb7024a74f1a
Pavel Shramov <psha@nguni.psha.org.ru>
2024-06-21 14:13:58 +0300

    bench: Measure object binding time

--- bench/bench.sh
+++ bench/bench.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-exec ~/src/tll/build/bench/bench-channel -m build/tll-lua --count=1000000 --msgid=10 \
+exec ~/src/tll/build/bench/tll-bench-channel -m build/tll-lua --count=1000000 --msgid=10 \
 	'lua+null://;code=file://bench/forward.lua;scheme=yaml://bench/scheme.yaml' \
 	'lua+null://;code=file://bench/table.lua;scheme=yaml://bench/scheme.yaml' \
 	'lua+null://;code=file://bench/simple.lua;scheme=yaml://bench/scheme.yaml' \
 	'lua+null://;code=file://bench/nested.lua;scheme=yaml://bench/scheme.yaml' \
 	'lua+null://;code=file://bench/many.lua;scheme=yaml://bench/scheme.yaml' \
-	'lua+null://;code=file://bench/static.lua;scheme=yaml://bench/scheme.yaml'
+	'lua+null://;code=file://bench/static.lua;scheme=yaml://bench/scheme.yaml' \
+	'lua+null://;code=file://bench/object.lua;scheme=yaml://bench/scheme.yaml;lua.message-mode=object'
